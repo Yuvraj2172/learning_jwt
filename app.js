c5553ec671e877067d6152484c5f52e005859457
Yuvraj Soni <demonsroar2172@gmail.com>
2024-02-15 14:23:55 +0530

    sign token

--- app.js
+++ app.js
@@ -4,17 +4,16 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+const mainRouter = require("./routes/main");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-// main router
-const mainRouter = require("./routes/main");
-
-app.use("/api/v1", mainRouter);
 // middleware
 app.use(express.static("./public"));
 app.use(express.json());
+app.use("/api/v1", mainRouter);
 
+// main router
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
--- controllers/main.js
+++ controllers/main.js
@@ -1,13 +1,30 @@
-const login = (req, res) => {
-  res.send("Invalid login/signup/ register route");
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw new CustomAPIError("Invalid username or password", 400);
+  }
+  //just for demon, generally provided by the DB
+  const id = new Date().getTime();
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "15days",
+  });
+  res.status(200).json({
+    msg: "user created",
+    token,
+  });
 };
 
-const dashboard = (req, res) => {
+const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);
+  //   console.log('route is working fine');
   res.status(200).json({
     msg: `Hello Yuvraj Soni!`,
     secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
   });
+  console.log("working fine");
 };
 
 module.exports = {
